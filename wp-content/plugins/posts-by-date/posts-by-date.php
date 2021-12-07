d452cd6d5214ac531446ef81035c854660c49126
Nima Rahbar <nima.rahbar@gmail.com>
2021-12-07 19:57:46 +0200

    Create Plugin Main PHP File

---
+++ wp-content/plugins/posts-by-date/posts-by-date.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name:     Posts By Date
+ * Plugin URI:      https://bettercollective.com
+ * Description:     It will be used to display a list of posts from certain categories with date and limit filter.
+ * Author:          Nima Rahbar
+ * Author URI:      https://nimarahbar.com
+ * Text Domain:     posts-by-date
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Posts_By_Date
+ */
+
