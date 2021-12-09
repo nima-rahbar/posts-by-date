76a519d78630d6db26d7862a51be0bbe60d1d803
Nima Rahbar <nima.rahbar@gmail.com>
2021-12-09 23:04:20 +0200

    Load More functionality is done

--- wp-content/plugins/posts-by-date/assets/js/posts-by-date.js
+++ wp-content/plugins/posts-by-date/assets/js/posts-by-date.js
@@ -1,5 +1,37 @@
 (function ($) {
+    "use strict";
     $(document).ready(function () {
-        
+        $("#load-more").on('click', function (e) {
+            // var $(this) = $(this);
+            $(this).attr('disabled', true);
+            $.ajax({
+                type : "post",
+                url: myAjax.ajaxurl,
+                dataType: 'json',
+                data: {
+                    action : 'load_more',
+                    args: {
+                        category: $("#load-more").data('category'), 
+                        date: $("#load-more").data('date'),
+                        limit: $("#load-more").data('limit'),
+                        orderby: $("#load-more").data('orderby'),
+                        order: $("#load-more").data('order'),
+                        current: $("#load-more").attr('data-paged'),
+                        output: ''
+                    }
+                },
+                success: function (response) {
+                    if(response.status == 'success'){
+                        $("#load-more").removeAttr('disabled').attr('data-paged', response.current).before(response.output);
+                    }else{
+                        $("#load-more").before(response.output);
+                        $("#load-more").slideUp(600, function () { 
+                            $(this).remove();
+                            $("body").find("#no-more-post").slideDown().delay(4000).slideUp();
+                         });
+                    }
+                }
+            });
+        });
     });
 })(jQuery)
--- wp-content/plugins/posts-by-date/posts-by-date.php
+++ wp-content/plugins/posts-by-date/posts-by-date.php
@@ -16,23 +16,33 @@ class PostsByDate
 {
     public function __construct()
     {
-        if(!is_admin()){
+        if (!is_admin()) {
             add_action('init', array($this, 'posts_by_date_enqueue_scripts'));
             add_shortcode('posts_by_date', array($this, 'posts_by_date_shortcode'));
         }
+        add_action('wp_ajax_load_more', array($this, 'load_more'));
+        add_action('wp_ajax_nopriv_load_more', array($this, 'load_more'));
     }
 
     public function posts_by_date_enqueue_scripts_admin()
     {
         wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', false, '5.1.3', 'all');
         wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css', false, '1.7.0', 'all');
-        wp_enqueue_style('posts-by-date', plugin_dir_url(__FILE__) . 'assets/css/posts-by-date.css', false, time(), 'all');
         wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js', array('jquery'), '5.1.3', true);
         wp_enqueue_script('posts-by-date', plugin_dir_url(__FILE__) . 'assets/js/posts-by-date-admin.js', array('jquery', 'jquery-ui-core', 'jquery-ui-slider', 'jquery-ui-spinner'), time(), true);
     }
     public function posts_by_date_enqueue_scripts()
     {
-        wp_enqueue_script('posts-by-date', plugin_dir_url(__FILE__) . 'assets/js/posts-by-date.js', array('jquery'), time(), true);
+        wp_enqueue_style('posts-by-date', plugin_dir_url(__FILE__) . 'assets/css/posts-by-date.css', false, time(), 'all');
+        wp_register_script('posts-by-date', plugin_dir_url(__FILE__) . 'assets/js/posts-by-date.js', array('jquery'), time(), true);
+        wp_localize_script(
+            'posts-by-date',
+            'myAjax',
+            array(
+                'ajaxurl' => admin_url('admin-ajax.php')
+            )
+        );
+        wp_enqueue_script('posts-by-date');
     }
 
     public function posts_by_date_shortcode($atts)
@@ -55,6 +65,7 @@ class PostsByDate
         $orderby = $db_options['orderby_3'];
         $order = $db_options['order_4'];
 
+
         if (empty($category)) {
             return 'You should select at least one category.';
         }
@@ -62,7 +73,7 @@ class PostsByDate
             'year' => date("Y", strtotime($date)),
             'month' => date("m", strtotime($date)),
             'day' => date("d", strtotime($date))
-            ) : array();
+        ) : array();
 
         $args = array(
             'post_type' => 'post',
@@ -74,26 +85,82 @@ class PostsByDate
             'order' => $order,
             'date_query' => $date_query,
         );
+
         $posts = new WP_Query($args);
-        if($posts->have_posts()){
-            $o = '<ol>';
-            while($posts->have_posts()){
+        if ($posts->have_posts()) {
+            $o = '<ol id="posts-by-date">';
+            while ($posts->have_posts()) {
                 $posts->the_post();
                 $o .= '<li><ul>';
-                $o .= "<li>".get_the_title()."</li>";
-                $o .= "<li>".get_the_excerpt()."</li>";
-                $o .= "<li>".get_the_date("Y-m-d")."</li>";
+                $o .= "<li><b>" . get_the_title() . "</b> - <small><i>" . get_the_date("Y-m-d") . "</i></small></li>";
+                $o .= "<li><small>" . wp_trim_words(get_the_excerpt(), 30) . "</small></li>";
                 $o .= '</ul></li>';
             }
-            $o .= '</ol>';
             wp_reset_postdata();
-            // if($posts->found_posts > $limit){
-            //     $o .= '<button id="load-more" data-page="1">Load More!</button>';
-            // }
-        }else{
+            if ($posts->found_posts > $limit) {
+                $o .= "<button id=\"load-more\" data-category=\"$category\" data-limit=\"$limit\" data-date=\"$date\" data-orderby=\"$orderby\" data-order=\"$order\" data-paged=\"1\">Load More!</button>";
+            }
+            $o .= '</ol>';
+        } else {
             return 'There is no post with your settings in our Database.';
         }
         return $o;
+    }
+
+    public function load_more()
+    {
+        $params = $_POST['args'];
+
+        $date_query = (!empty($params['date'])) ? array(
+            'year' => date("Y", strtotime($params['date'])),
+            'month' => date("m", strtotime($params['date'])),
+            'day' => date("d", strtotime($params['date']))
+        ) : array();
+
+        $args = array(
+            'post_type' => 'post',
+            'category_name' => $params['category'],
+            'post_status' => 'publish',
+            'posts_per_page' => $params['limit'],
+            'paged' => ($params['current'] + 1),
+            'orderby' => $params['orderby'],
+            'order' => $params['order'],
+            'date_query' => $date_query,
+        );
+        $posts = new WP_Query($args);
+        if ($posts->have_posts() && ($posts->found_posts > ($params['current']) * $params['limit'])) {
+            $o = '';
+            while ($posts->have_posts()) {
+                $posts->the_post();
+                $o .= '<li><ul>';
+                $o .= "<li><b>" . get_the_title() . "</b> - <small><i>" . get_the_date("Y-m-d") . "</i></small></li>";
+                $o .= "<li><small>" . wp_trim_words(get_the_excerpt(), 30) . "</small></li>";
+                $o .= '</ul></li>';
+            }
+            wp_reset_postdata();
+            $response = array(
+                'status'   => 'success',
+                'output' => $o,
+                'current' => $params['current'] + 1
+            );
+        } else {
+            $response = array(
+                'status'   => 'finished',
+                'output' => '<div id="no-more-post"><b>There is no more posts to load.</b></div>',
+            );
+        }
+        echo json_encode($response);
+        wp_die();
+    }
+
+    public function debug_array($array, $is_for_admin_area = false)
+    {
+        if ($is_for_admin_area) {
+            $css = 'text-align: left; margin-left:200px; position:absolute; z-index: 100;';
+        } else {
+            $css = "text-align: left;";
+        }
+        echo '<pre style="' . $css . '" >', print_r($array, true), '</pre>';
     }
 }
 require_once plugin_dir_path(__FILE__) . 'includes' . DIRECTORY_SEPARATOR . 'admin-menu.php';
